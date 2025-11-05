30545e997aeacac4efa0fd769fe5a9bee0ffbd6c
Alan Chien <chien.alan.dev@gmail.com>
2025-11-05 09:55:45 -0500

    Complete comparisons comprehension tasks

---
+++ JS_Fundamentals/comparison.js
@@ -0,0 +1,9 @@
+// What will be the result for these expressions?
+
+console.log(5 > 4); //true
+console.log("apple" > "pineapple"); //false, p > a
+console.log("2" > "12"); //true - no conversion since same type
+console.log(undefined == null); //true, "sweet couple"
+console.log(undefined === null); //false, different types
+console.log(null == "\n0\n"); //false, no conversion
+console.log(null === +"\n0\n");//false, different types
